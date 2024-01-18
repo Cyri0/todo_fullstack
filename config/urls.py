d6e7f52caf2b67474fb61868916dbef828ca44d5
Nemes Tamás <nemes.tamas@mechwart.local>
2024-01-18 17:05:16 +0100

    base project created

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.shortcuts import render
+
+def main(request):
+    return render(request, 'index.html')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main),
 ]
